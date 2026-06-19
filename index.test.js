b92551cdc75ae0f999fec06b1ba61b38c800def4
Vicodev <quinterodev_09@outlook.com>
2026-06-18 17:22:24 -0700

    actulizar test

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ import assert from "node:assert";
 import { sumar, esPar } from "./index.js";
 
 test("sumar 2 + 3 debe dar 5", () => {
-  assert.strictEqual(sumar(2, 3), 5);
+  assert.strictEqual(sumar(2, 1), 5);
 });
 
 test("esPar(4) debe dar true", () => {
